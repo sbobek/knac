6924e2696daddf0f62afc2a567f7284308ee7b5c
Szymon Bobek <szymon.bobek@uj.edu.pl>
2021-09-22 23:01:31 +0200

    removed min max scaling from split matrix

--- knac/knac.py
+++ knac/knac.py
@@ -103,7 +103,7 @@ class KNAC:
         
         self.H_conf2 = normalize(X, axis=0)
         self.H_conf2 = self.H_conf2 * 1.0 / (np.array(self.H)/np.log2(len(X))+1) 
-        self.H_conf2 = minmax_scale(self.H_conf2, axis=1)
+        #self.H_conf2 = minmax_scale(self.H_conf2, axis=1)
         self.H_conf2 = pd.DataFrame(self.H_conf2, index=X.index, columns=X.columns)
 
         z = X.sum(axis=1)
